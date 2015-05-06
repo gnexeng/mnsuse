c2bed31df5c22e257693c827faf2bae7b452a824
gnexeng <gnexeng@gmail.com>
2015-05-06 10:49:37 -0500

    first
    This is the first code pushed to GitHub.

---
+++ mnsuse/mnsuse.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace mnsuse
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+//			MainPage = new ContentPage {
+//				Content = new StackLayout {
+//					VerticalOptions = LayoutOptions.Center,
+//					Children = {
+//						new Label {
+//							XAlign = TextAlignment.Center,
+//							Text = "Welcome to Xamarin Forms!"
+//						}
+//					}
+//				}
+//			};
+
+			MainPage = new ContentPage{
+				Content = new ScrollView{
+					Content = new StackLayout{
+						Orientation = StackOrientation.Vertical,
+						HorizontalOptions = LayoutOptions.Start,
+						Padding = new Thickness(20),
+						Spacing = 10,
+						Children = {
+							new msuStackItems().createLabel("Label 1", Color.Yellow, 18),
+							new msuStackItems().createLabel("Label 2", Color.Blue, 18),
+							new msuStackItems().createLabel("Label 1", Color.Yellow, 18),
+							new msuStackItems().createLabel("Label 2", Color.Blue, 18),
+							new msuStackItems().createLabel("Label 1", Color.Yellow, 18),
+							new msuStackItems().createLabel("Label 2", Color.Blue, 18),
+							new msuStackItems().createLabel("Label 1", Color.Yellow, 18),
+							new msuStackItems().createLabel("Label 2", Color.Blue, 18),
+							new msuStackItems().createLabel("Label 1", Color.Yellow, 18),
+							new msuStackItems().createLabel("Label 2", Color.Blue, 18),
+							new msuStackItems().createLabel("Label 1", Color.Yellow, 18),
+							new msuStackItems().createLabel("Label 2", Color.Blue, 18),
+							new msuStackItems().createLabel("Label 1", Color.Yellow, 18),
+							new msuStackItems().createLabel("Label 2", Color.Blue, 18),
+							new msuStackItems().createLabel("Label 1", Color.Yellow, 18),
+							new msuStackItems().createLabel("Label 2", Color.Blue, 18),
+							new msuStackItems().createLabel("Label 1", Color.Yellow, 18),
+							new msuStackItems().createLabel("Label 2", Color.Blue, 18),
+							new msuStackItems().createLabel("Label 1", Color.Yellow, 18),
+							new msuStackItems().createLabel("Label 2", Color.Blue, 18),
+
+						}
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ mnsuse/msuStackItems.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace mnsuse
+{
+	public class msuStackItems: View
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="mnsuse.msuStackItems"/> class.
+		/// To create items that can be used as children of a StackView
+		/// </summary>
+		public msuStackItems ()
+		{
+		}
+
+		public Label createLabel(string text, Color clr, double fontSize)
+		{
+			Label label = new Label {
+				Text = text,
+				BackgroundColor = clr,
+				WidthRequest = 200  //number of character spaces
+			};
+			label.FontSize = fontSize;
+			return label;
+		}
+	}
+}
+
